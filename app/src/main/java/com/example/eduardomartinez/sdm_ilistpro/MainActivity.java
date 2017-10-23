b5acfa155ff9e5c4635f45be1f135cfefd1058fc
Unioviedu <UO244594@uniovi.es>
2017-10-23 21:25:08 +0200

    Primer commit con el esqueleto y el gitignore

---
+++ app/src/main/java/com/example/eduardomartinez/sdm_ilistpro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eduardomartinez.sdm_ilistpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
